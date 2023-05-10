0a12baf696c209cff2fbfece713f52c41fc63c12
Nwosa <obere4u@gmail.com>
2023-05-10 16:17:19 +0100

    Added README file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,13 +7,16 @@ function About() {
       <p className="my__card-about__text">
         Experienced <strong>React</strong> Developer proficient in{" "}
         <strong>HTML</strong>, <strong>CSS</strong>, <strong>Tailwind CSS</strong>, and
-         <strong>JavaScript</strong>. Strong passion for creating functional and visually
+         <strong>JavaScript</strong>. 
+         Strong passion for creating functional and visually
         appealing web applications using the latest technologies and best
-        practices. Solid understanding of React's core concepts and experienced
+        practices. 
+        Solid understanding of React's core concepts and experienced
         in working with Redux for state management. Collaborative and
         communicative with a track record of delivering high-quality code that
-        meets clients' needs. Excited about continuously improving skills and
-        exploring new technologies.
+        meets clients' needs. 
+        Excited about continuously improving skills and
+        exploring new technologies....
       </p>
     </div>
   );
